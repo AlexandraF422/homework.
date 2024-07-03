3ca2a8bab607c77cff02806e7d4b2f0935fd3f0a
AlexandraF422 <bill95@mail.ru>
2024-07-03 16:19:00 +0300

    homework2.

---
+++ main.py
@@ -0,0 +1,5 @@
+home_work = 12
+time_spent = 1.5
+course_name = "python"
+time_for_one_task = (12/1.5)
+print("Курс:",course_name,",всего задач:",home_work,",затрачено часов:",time_spent,",среднее время выполнение",time_for_one_task)
