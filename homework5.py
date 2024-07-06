7fd857a929f5730767c1d40ecfe31b4130f8c88c
AlexandraF422 <bill95@mail.ru>
2024-07-06 23:48:37 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 1,2,'a','b'
+print(immutable_var)
+#immutable_var [2]=13
+#print(immutable_var)
+#является не изменным кортежем
+mutable_list = ([1,2,'a','true'],3,'b')
+mutable_list [0][2] = 15
+print(mutable_list)
