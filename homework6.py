92df9aff2e6a51d2a4c418fe4209044b5fd51c88
AlexandraF422 <bill95@mail.ru>
2024-07-08 21:26:12 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,17 @@
+my_dict = {'Alex': 1995, 'Sasha': 1997, 'Misha': 1998}
+print(my_dict)
+my_dict ['Vika'] = 2001
+print(my_dict)
+my_dict.update({'Katy': 1989,
+                'Oly': 1976})
+print(my_dict)
+a = my_dict.pop('Sasha')
+print(a)
+print(my_dict)
+
+my_set ={1,3,5,8,4,3,1,'String',True}
+print(my_set)
+my_set.add(13)
+my_set.add(89)
+my_set.discard(3)
+print(my_set)
