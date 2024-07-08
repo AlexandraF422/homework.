e6ba9e68ec7ae36d7a698b7623cbb6824902be48
AlexandraF422 <bill95@mail.ru>
2024-07-08 11:13:06 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -3,6 +3,6 @@ print(immutable_var)
 #immutable_var [2]=13
 #print(immutable_var)
 #является не изменным кортежем
-mutable_list = ([1,2,'a','true'],3,'b')
-mutable_list [0][2] = 15
+mutable_list = [1,2,'a','true',3,'b']
+mutable_list [2] = 15
 print(mutable_list)
