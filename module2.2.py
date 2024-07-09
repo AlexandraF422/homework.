fa9822500b6b840ab6a8d702916fd27b2e81cfdd
AlexandraF422 <bill95@mail.ru>
2024-07-09 20:48:18 +0300

    Add files via upload

---
+++ module2.2.py
@@ -0,0 +1,9 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+if first==second or second==third or third==first:
+    print(2)
+if first!=second!=third:
+    print(0)
