038e6b933ed8dabddec06017509b3982fae90f6a
AlexandraF422 <bill95@mail.ru>
2024-07-24 12:48:02 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,11 @@
+result = []
+n = int(input("Число "))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if n % (i +j) == 0 and j > i:
+            result.append((i))
+            result.append(j)
+result1 = ''
+for p in result:
+    result1 += str(p)
+print('Шифр: ', result1)
