315a8ae62c03a33dc334c7d18ac292be8954f6e9
AlexandraF422 <bill95@mail.ru>
2025-01-19 18:45:21 +0300

    Add files via upload

---
+++ module7.4.py
@@ -0,0 +1,27 @@
+team1_num = 5
+team2_num = 6
+
+print("В команде Мастера кода участников: %s" % (team1_num) )
+print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+
+team1_time = 1552.512
+score_2 = 42
+
+print("Команда Волшебники данных решила задач: {}" .format(score_2))
+print("Волшебники данных решили задачи за {}" .format(team1_time))
+
+score_1 = 40
+score_2 = 42
+challenge_result = "Победа команды Волшебники данных!"
+tasks_total = 82
+time_avg = 45.2
+
+print(f"Команды решили {score_1} и {score_2} задач.")
+print(f"Результат битвы: {challenge_result}")
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
+
+
+
+
+
+
